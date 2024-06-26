7ab238223f7675b4826035ffbd389ed557ddc319
Nyxchan08 <christiandv.moreno@gmail.com>
2024-06-26 20:33:27 +0800

    trying to make it available online

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     // LOG IN
 
     public function login(){
-        return view('login.index');
+        return view('views.index');
     }
 
     public function processLogin(Request $request)
